97116437dc92ed0d0f963a890f72dbe0339914da
gbs4ever <gbs4ever2@gmail.com>
2019-10-17 01:00:34 -0400

    changed footer icon,added linkden

--- src/components/theme/Footer/index.jsx
+++ src/components/theme/Footer/index.jsx
@@ -7,20 +7,12 @@ export const Footer = () => (
 	<Wrapper>
 		<Flex as={Container}>
 			<Details>
-				<h2>John Doe</h2>
+				<h2>Simcha Greenbaum</h2>
 				<span>
 					© All rights are reserved | {new Date().getFullYear()} | Made with{' '}
 					<span aria-label="love" role="img">
 						💖
 					</span>{' '}
-					by{' '}
-					<a
-						href="https://smakosh.com/?ref=portfolio-dev"
-						rel="noopener noreferrer"
-						target="_blank"
-					>
-						Smakosh
-					</a>
 				</span>
 			</Details>
 			<Links>
