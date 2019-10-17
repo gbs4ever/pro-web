8cb06cc441d8de4419a2a88e64a6b5c0e3a27d68
gbs4ever <gbs4ever2@gmail.com>
2019-10-17 01:02:25 -0400

    changed skills section

--- src/components/landing/Skills/index.jsx
+++ src/components/landing/Skills/index.jsx
@@ -8,14 +8,21 @@ export const Skills = () => (
 	<Wrapper id="about">
 		<SkillsWrapper as={Container}>
 			<Thumbnail>
-				<img src={dev} alt="I’m John and I’m a Backend & Devops engineer!" />
+				<img src={dev} alt="I’m Sim and I’m a Backend & Software  engineer!" />
 			</Thumbnail>
 			<Details>
-				<h1>Hi There!</h1>
 				<p>
-					Lorem Ipsum is simply dummy text of the printing and typesetting
-					industry. Lorem Ipsum has been the industry’s standard dummy.
+					I'm a Software engineer with 2+ years of technical experience
+					developing corporate systems that drive productivity, and increase
+					revenue as well as overall success.
 				</p>
+
+				<h3>
+					Skills:
+					<span>
+						Ruby, Rails, PostgreSQL, WSL, JavaScript, jQuery, React, Redux
+					</span>
+				</h3>
 				<Button as={AnchorLink} href="#contact">
 					Hire me
 				</Button>
