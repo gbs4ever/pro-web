d9b852135a644793e5fd559e185ec388a86b3085
gbs4ever <gbs4ever2@gmail.com>
2019-11-01 13:25:27 -0400

    new thumail

--- src/components/common/SEO/index.jsx
+++ src/components/common/SEO/index.jsx
@@ -62,7 +62,7 @@ export const SEO = ({
 			<meta property="og:type" content="website" />
 			<meta property="og:title" content={title} />
 			<meta property="og:description" content={description} />
-			<meta property="og:image" content="www.simchagreenbaum.dev" />
+			<meta property="og:image" content={Thumbnail} />
 			{/* <meta property="fb:app_id" content={social.facebook} /> */}
 
 			<meta name="twitter:card" content="summary" />
