e22a139f61c3d7a7658435facabd7c0dba4ab591
gbs4ever <gbs4ever2@gmail.com>
2019-11-08 01:13:27 -0500

    import css bootstrap

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,6 @@
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import './node_modules/bootstrap-css-only/css/bootstrap.min.css'
+
+import './node_modules/mdbreact/dist/css/mdb.css'
+
 export const onServiceWorkerUpdateReady = () => window.location.reload(true)
