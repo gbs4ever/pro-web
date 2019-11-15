7415442594d6a677f3ea8e009ad70be72b33a22a
gbs4ever <gbs4ever2@gmail.com>
2019-11-15 15:06:05 -0500

    dont display private repos

--- src/components/landing/Projects/index.jsx
+++ src/components/landing/Projects/index.jsx
@@ -6,68 +6,75 @@ import forkIcon from 'Static/icons/fork.svg'
 import { Wrapper, Grid, Item, Content, Stats } from './styles'
 
 export const Projects = () => {
-	const {
-		github: {
-			repositoryOwner: {
-				repositories: { edges },
-			},
-		},
-	} = useStaticQuery(graphql`
-		{
-			github {
-				repositoryOwner(login: "gbs4ever") {
-					repositories(
-						first: 6
-						orderBy: { field: STARGAZERS, direction: DESC }
-					) {
-						edges {
-							node {
-								id
-								name
-								url
-								description
-								stargazers {
-									totalCount
-								}
-								forkCount
-							}
-						}
-					}
-				}
-			}
-		}
-	`)
-	return (
-		<Wrapper as={Container} id="projects">
-			<h2>Projects</h2>
-			<Grid>
-				{edges.map(({ node }) => (
-					<Item
-						key={node.id}
-						as="a"
-						href={node.url}
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						<Card>
-							<Content>
-								<h4>{node.name}</h4>
-								<p>{node.description}</p>
-							</Content>
-							<Stats>
-								<div>
-									<img src={starIcon} alt="stars" />
-									<span>{node.stargazers.totalCount}</span>
-								</div>
-								<div>
-									<img src={forkIcon} alt="forks" />
-									<span>{node.forkCount}</span>
-								</div>
-							</Stats>
-						</Card>
-					</Item>
-				))}
-			</Grid>
-		</Wrapper>
-	)
+  const {
+    github: {
+      repositoryOwner: {
+        repositories: { edges },
+      },
+    },
+  } = useStaticQuery(graphql`
+    {
+      github {
+        repositoryOwner(login: "gbs4ever") {
+          repositories(
+            first: 6
+
+            orderBy: { field: STARGAZERS, direction: DESC }
+          ) {
+            edges {
+              node {
+                id
+                isPrivate
+                name
+                url
+                description
+                stargazers {
+                  totalCount
+                }
+                forkCount
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <Wrapper as={Container} id="projects">
+      <h2>Projects</h2>
+      <Grid>
+        {console.log(edges)}
+        {edges.map(({ node }) =>
+          node.isPrivate == true ? (
+            console.log(node)
+          ) : (
+            <Item
+              key={node.id}
+              as="a"
+              href={node.url}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Card>
+                <Content>
+                  <h4>{node.name}</h4>
+                  <p>{node.description}</p>
+                </Content>
+                <Stats>
+                  <div>
+                    <img src={starIcon} alt="stars" />
+                    <span>{node.stargazers.totalCount}</span>
+                  </div>
+                  <div>
+                    <img src={forkIcon} alt="forks" />
+                    <span>{node.forkCount}</span>
+                  </div>
+                </Stats>
+              </Card>
+            </Item>
+          )
+        )}
+      </Grid>
+    </Wrapper>
+  )
 }
