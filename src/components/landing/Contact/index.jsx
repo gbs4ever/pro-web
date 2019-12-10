d6810c93216beaf2a89d790d98cf4757200ade57
gbs4ever <gbs4ever2@gmail.com>
2019-12-10 16:38:14 -0500

    update contact pic

--- src/components/landing/Contact/index.jsx
+++ src/components/landing/Contact/index.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import { Container } from 'Common'
-import contact from 'Static/illustrations/contact.svg'
+import contact from 'Static/illustrations/apps-dev.svg'
 import { Wrapper, Details, Thumbnail } from './styles'
 import ContactForm from './ContactForm'
 
 export const Contact = () => (
-	<Wrapper as={Container} id="contact">
-		<Details>
-			<ContactForm />
-		</Details>
-		<Thumbnail>
-			<img src={contact} alt="I’m Sim and I’m a Backend engineer!" />
-		</Thumbnail>
-	</Wrapper>
+  <Wrapper as={Container} id="contact">
+    <Details>
+      <ContactForm />
+    </Details>
+    <Thumbnail>
+      <img src={contact} alt="I’m Sim and I’m a Backend engineer!" />
+    </Thumbnail>
+  </Wrapper>
 )
