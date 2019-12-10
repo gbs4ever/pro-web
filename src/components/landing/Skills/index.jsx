72da2185c5f562db27506a5257a7c8a5f1e3c5ad
gbs4ever <gbs4ever2@gmail.com>
2019-12-10 17:52:01 -0500

    wording for skills

--- src/components/landing/Skills/index.jsx
+++ src/components/landing/Skills/index.jsx
@@ -5,27 +5,27 @@ import dev from 'Static/illustrations/skills.svg'
 import { Wrapper, SkillsWrapper, Details, Thumbnail } from './styles'
 
 export const Skills = () => (
-	<Wrapper id="about">
-		<SkillsWrapper as={Container}>
-			<Thumbnail>
-				<img src={dev} alt="I’m Sim and I’m a Backend & Software  engineer!" />
-			</Thumbnail>
-			<Details>
-				<p>
-					I'm a Software engineer with 2+ years of technical experience
-					developing corporate systems that drive productivity, and increase
-					revenue as well as overall success.
-				</p>
+  <Wrapper id="about">
+    <SkillsWrapper as={Container}>
+      <Thumbnail>
+        <img src={dev} alt="I’m Sim and I’m a Backend & Software  engineer!" />
+      </Thumbnail>
+      <Details>
+        <p>
+          I'm a Software engineer with 2+ years of technical experience
+          developing corporate systems that drive productivity, and increase
+          revenue as well as overall success.
+        </p>
 
-				<p>
-					<strong>Skills:</strong> Ruby, Rails, PostgreSQL, WSL, JavaScript,
-					jQuery, Python,Docker,React, Redux{' '}
-				</p>
+        <p>
+          <strong>Skills:</strong> Ruby, Rails, PostgreSQL, WSL, JavaScript,
+          jQuery, Python, Docker, React, Redux, HTML/CSS,{' '}
+        </p>
 
-				<Button as={AnchorLink} href="#contact">
-					Contact me
-				</Button>
-			</Details>
-		</SkillsWrapper>
-	</Wrapper>
+        <Button as={AnchorLink} href="#contact">
+          Contact me
+        </Button>
+      </Details>
+    </SkillsWrapper>
+  </Wrapper>
 )
