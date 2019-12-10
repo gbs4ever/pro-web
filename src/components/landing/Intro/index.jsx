77244a7e783d3663ebf4176b10c957109b1252fc
gbs4ever <gbs4ever2@gmail.com>
2019-12-10 15:25:17 -0500

    changed graphics

--- src/components/landing/Intro/index.jsx
+++ src/components/landing/Intro/index.jsx
@@ -2,26 +2,26 @@ import React from 'react'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 import { Header } from 'Theme'
 import { Container, Button } from 'Common'
-import dev from 'Static/illustrations/dev.svg'
+import dev1 from 'Static/illustrations/dev1.svg'
 import { Wrapper, IntroWrapper, Details, Thumbnail } from './styles'
 
 export const Intro = () => (
-	<Wrapper>
-		<Header />
-		<IntroWrapper as={Container}>
-			<Details>
-				<h2>
-					Do you need someone who can help take your technological needs to the
-					next level?
-				</h2>
-				<h4 />
-				<Button as={AnchorLink} href="#contact">
-					Hire me
-				</Button>
-			</Details>
-			<Thumbnail>
-				<img src={dev} alt="I’m Sim and I’m a Full Stack Engineer!" />
-			</Thumbnail>
-		</IntroWrapper>
-	</Wrapper>
+  <Wrapper>
+    <Header />
+    <IntroWrapper as={Container}>
+      <Details>
+        <h2>
+          Do you need someone who can help take your technological needs to the
+          next level?
+        </h2>
+        <h4 />
+        <Button as={AnchorLink} href="#contact">
+          Hire me
+        </Button>
+      </Details>
+      <Thumbnail>
+        <img src={dev1} alt="I’m Sim and I’m a Full Stack Engineer!" />
+      </Thumbnail>
+    </IntroWrapper>
+  </Wrapper>
 )
