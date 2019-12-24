0f26ed7758a2eef0ceb42c538bbedc0f4d6cb668
gbs4ever <gbs4ever2@gmail.com>
2019-12-23 22:11:16 -0500

    remove unused fonts

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,4 @@
-import '@fontawesome/fontawesome-free/css/all.min.css'
+// import '@fortawesome/fontawesome-free/css/all.min.css'
 import './node_modules/bootstrap-css-only/css/bootstrap.min.css'
 
 import './node_modules/mdbreact/dist/css/mdb.css'
