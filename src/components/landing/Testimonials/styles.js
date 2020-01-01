4fdddb8675d9dd96bc3f8eda4d86a3ca5df50913
gbs4ever <gbs4ever2@gmail.com>
2020-01-01 00:48:20 -0500

    added 3rd testimonial styles

--- src/components/landing/Testimonials/styles.js
+++ src/components/landing/Testimonials/styles.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components'
 
 export const Details = styled.div`
-	padding: 10px;
-	h2:hover {
-		color: #1cb495 !important;
-	}
+  padding: 10px;
+  h2:hover {
+    color: #1cb495 !important;
+  }
 `
