1b6b6b58ff7a168fdacff70e10cdcbe59c847b36
gbs4ever <gbs4ever2@gmail.com>
2023-04-17 21:21:21 -0400

    fixed wrong file input

--- src/components/theme/Header/NavbarLinks/index.jsx
+++ src/components/theme/Header/NavbarLinks/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 import pdf from './SGreenbaum_Resume_2023_1.pdf'
-import { Wrapper } from '../../styles'
+import { Wrapper } from './styles'
 
 const NavbarLinks = ({ desktop }) => (
   <Wrapper desktop={desktop}>
